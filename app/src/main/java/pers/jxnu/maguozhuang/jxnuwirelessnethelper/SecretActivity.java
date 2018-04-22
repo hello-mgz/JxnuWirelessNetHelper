e4e536d28c448626a585f64a7c2c8afde3992d9b
mgzgithub <757033360@qq.com>
2018-04-22 20:50:20 +0800

    1.2.1（beta）

---
+++ app/src/main/java/pers/jxnu/maguozhuang/jxnuwirelessnethelper/SecretActivity.java
@@ -0,0 +1,25 @@
+package pers.jxnu.maguozhuang.jxnuwirelessnethelper;
+
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecretActivity extends AppCompatActivity
+{
+    private TextView secret_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(pers.jxnu.maguozhuang.jxnuwirelessnethelper.R.layout.activity_secret);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null)
+            actionBar.hide();
+        secret_text=(TextView)findViewById(pers.jxnu.maguozhuang.jxnuwirelessnethelper.R.id.secret_text);
+        Typeface typeface=Typeface.createFromAsset(getAssets(),"fonts/myfont.ttf");
+        secret_text.setTypeface(typeface);
+    }
+}
